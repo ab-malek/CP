586af12933abba72d171a6d18e2e3bece6024d75
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-08-21 16:23:47 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,25 @@
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
 #define ll long long
 #define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
 #define all(a) a.begin(),a.end()
-#define all(a) a.begin(),a.end()
-#define forz(i,n) for(int i=0;i<n;i++)
-#define foro(i,n) for(int i=1;i<=n;i++)
+#define flush fflush(stdout) 
+#define int long long
+const long long INF = 1e18;
+#define check cout<<"ok"<<nl
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;// find_by_order, order_of_key
+#define printVec(v) for(int i=0;i<v.size();i++){cout<<v[i]<<" ";}cout<<nl
+#define printVecPair(v) for(auto it : v){cout<<it.first<<" "<<it.second<<nl;}
+const int N = 1e9 + 7;
+const int inf = 1e9;
+
 
 
 ///////   nCr   inverse mod    //////
