f3541f4b0232610f025b8ccc6dd35cfe4a5c5dcf
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-08-21 16:25:12 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define all(a) a.begin(),a.end()
+#define flush fflush(stdout) 
+#define int long long
+const long long INF = 1e18;
+#define check cout<<"ok"<<nl
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;// find_by_order, order_of_key
+#define printVec(v) for(int i=0;i<v.size();i++){cout<<v[i]<<" ";}cout<<nl
+#define printVecPair(v) for(auto it : v){cout<<it.first<<" "<<it.second<<nl;}
+const int N = 1e9 + 7;
+const int inf = 1e9;
+
+
+void solve(){
+    
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
