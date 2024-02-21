142f593249cd08f6133bf98547e3dad41969a437
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2024-02-21 23:20:50 +0600

    Update trie - find_differnt_subarray.cpp

--- Model question/trie - find_differnt_subarray.cpp
+++ Model question/trie - find_differnt_subarray.cpp
@@ -1,57 +1,77 @@
-int power(int a,int b,int mod){
-    int ans = 1%mod;;
-    a %= mod;
-    if(a<0) a+= mod;
-    while(b>0){
-        if(b&1) ans = (ans*a)%mod;
-        a = (a*a)%mod;
-        b >>= 1;
-    }
-    return ans;
-}
+Question link  : https://codeforces.com/contest/271/problem/D
 
-const int MOD1 = 127657753, MOD2 = 987654319;
-const int p1 = 137, p2 = 277;
-int ip1, ip2;
-pair<int, int> pw[N], ipw[N];
-void prec() {
-    pw[0] =  {1, 1};
-    for (int i = 1; i < N; i++) {
-        pw[i].first = 1LL * pw[i - 1].first * p1 % MOD1;
-        pw[i].second = 1LL * pw[i - 1].second * p2 % MOD2;
-    }
-    ip1 = power(p1, MOD1 - 2, MOD1);
-    ip2 = power(p2, MOD2 - 2, MOD2);
-    ipw[0] =  {1, 1};
-    for (int i = 1; i < N; i++) {
-        ipw[i].first = 1LL * ipw[i - 1].first * ip1 % MOD1;
-        ipw[i].second = 1LL * ipw[i - 1].second * ip2 % MOD2;
-    }
+#include<bits/stdc++.h>
+using namespace std;
 
-}
-struct Hashing {
-    int n;
-    vector<pair<int, int>> hash; // 1 - indexed
-    Hashing() {}
-    Hashing(const string& s) {
-        n = s.size();
-        hash.emplace_back(0, 0);
-        for (int i = 0; i < n; i++) {
-            pair<int, int> p;
-            p.first = (hash[i].first + 1LL * pw[i].first * s[i] % MOD1) % MOD1;
-            p.second = (hash[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD2;
-            hash.push_back(p);
-        }
-    }
-    pair<int, int> get_hash(int l, int r) { // 1 - indexed
-        assert(1 <= l && l <= r && r <= n);
-        pair<int, int> ans;
-        ans.first = (hash[r].first - hash[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
-        ans.second = (hash[r].second - hash[l - 1].second + MOD2) * 1LL * ipw[l - 1].second % MOD2;
-        return ans;
-    }
-    pair<int, int> get_hash() {
-        return get_hash();
-    }
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+  
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+//for unique type 'less'
+
+#define ll long long
+#define int long long
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define all(a) a.begin(),a.end()
+#define flush fflush(stdout) 
+const long long INF = 1e18;
+#define check cout<<"ok"<<nl
+#define printVec(v) for(int i=0;i<v.size();i++){cout<<v[i]<<" ";}cout<<nl
+#define printVecPair(v) for(auto it : v){cout<<it.first<<" "<<it.second<<nl;}
+// bool cmp(pair<int,int> &a, pair<int,int> &b){return a.second < b.second;}
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+const int mod = 998244353;
+const int N  = 2e5+5;
+
+struct node{
+	node *next[26];
+	node(){
+		for(int i=0;i<26;i++){
+			next[i] = NULL;
+		}
+	}
 };
-//call prec() , defind Hashing name(pass string); To get hash call sh.get_hash(l,r);
+
+void solve(){
+	string s,mark;
+	cin>>s>>mark;
+	int k;
+	cin>>k;
+	int n = s.size();
+	node *root = new node();
+	int res = 0;
+	for(int i=0;i<n;i++){
+		node *cur = root;
+		int bad = 0;
+		for(int j = i;j<n;j++){
+
+			bad += (mark[s[j] - 'a'] == '0');
+			if(bad > k) break;
+			int id = s[j] - 'a';
+			if(cur->next[id] == NULL){
+				res++;
+				cur->next[id] = new node();
+			}
+			cur = cur->next[id];
+		}
+	}
+
+	cout<<res<<nl;
+}	
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _t = 1;
+    // cin>>_t;
+    for(int i = 1;i<=_t;i++){
+    	// cout<<"Case "<<i<<": ";
+    	solve();
+    }
+}
