674decc328f3aa8155008f12ec465ff8e3757c0e
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2024-06-02 14:29:37 +0600

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,31 @@
+class DisjointSets {
+  private:
+	vector<int> parents;
+	vector<int> sizes;
+
+  public:
+  	// pass n+1
+	DisjointSets(int size){
+		parents.resize(size,-1);
+		sizes.resize(size,1);
+		for (int i = 0; i < size; i++) { parents[i] = i; }
+	}
+
+	int find(int x) {
+		return parents[x] == x ? x : (parents[x] = find(parents[x]));
+	}
+
+	bool unite(int x, int y) {
+		int x_root = find(x);
+		int y_root = find(y);
+		if (x_root == y_root) { return false; }
+
+		if (sizes[x_root] < sizes[y_root]) { swap(x_root, y_root); }
+		sizes[x_root] += sizes[y_root];
+		mxsizes = max(mxsizes, sizes[x_root]);
+		parents[y_root] = x_root;
+		return true;
+	}
+
+	bool connected(int x, int y) { return find(x) == find(y); }
+};
