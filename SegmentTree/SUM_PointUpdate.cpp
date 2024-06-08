406f2cc473e3f14609a9f6f0ccc8dec837684ec9
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2024-06-09 01:08:10 +0600

    Create SUM_PointUpdate.cpp

---
+++ SegmentTree/SUM_PointUpdate.cpp
@@ -0,0 +1,52 @@
+struct segmentTree {
+	vector<int> seg;
+
+	segmentTree(int n){
+		seg.resize(4*n);
+	}
+	void built_seg(int ind,int lo,int hi){
+		if(lo == hi){
+			seg[ind] = ar[lo];
+			return;
+		}
+		int mid = (lo+hi)/2;
+		built_seg(ind*2,lo,mid);
+		built_seg(ind*2+1,mid+1,hi);
+		seg[ind] = (seg[ind*2]+seg[ind*2+1]);
+	}
+	 
+	int query(int ind,int lo,int hi,int l,int r){
+		if(lo>r || hi<l || lo>hi) return 0;
+		if(lo>=l && hi<=r){
+			return seg[ind];
+		}
+		int mid = (lo+hi)/2;
+		int left = query(ind*2,lo,mid,l,r);
+		int right = query(ind*2+1,mid+1,hi,l,r);
+		return (left+right);
+	}
+	 
+	void pointUpdate(int ind,int lo,int hi,int node,int val){
+		if(lo == hi){
+			seg[ind] = val;
+			return ;
+		}
+		else{
+			int mid = (lo+hi) >> 1;
+			if(node<=mid && node>=lo){
+				pointUpdate(ind*2,lo,mid,node,val);
+			}
+			else{
+				pointUpdate(ind*2+1,mid+1,hi,node,val);
+			}
+			seg[ind] = seg[ind*2]+seg[ind*2+1];
+		}
+	}
+
+	int query(int l, int r){
+		return query(1,1,n,l,r);
+	}
+	void pointUpdate(int node, int val){
+		pointUpdate(1,1,n,node,val);
+	}
+};
