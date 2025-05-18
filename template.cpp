af49402503b9f7403c07c4fb05f023a1ea544287
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2025-05-18 15:51:15 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-  
-#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// #define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
 // for equal type 'less_equal'
 
 #define ll long long
@@ -14,9 +13,13 @@ using namespace __gnu_pbds;
 #define yes cout<<"YES\n"
 #define no cout<<"NO\n"
 #define all(a) a.begin(),a.end()
-#define flush fflush(stdout) 
+// #define flush fflush(stdout) 
 const long long INF = 1e18;
 #define check cout<<"ok"<<nl
+// #define mxheap priority_queue<int>
+// #define mnheap priority_queue<int, vector<int>, greater<int>>
+// #define mxheap2 priority_queue<pair<int,int>>
+// #define mnheap2 priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>
 #define printVec(v) for(int i=0;i<v.size();i++){cout<<v[i]<<" ";}cout<<nl
 #define printVecPair(v) for(auto it : v){cout<<it.first<<" "<<it.second<<nl;}
 // bool cmp(pair<int,int> &a, pair<int,int> &b){return a.second < b.second;}
@@ -24,24 +27,25 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define SetBit(x, k) (x |= (1LL << k))
 #define ClearBit(x, k) (x &= ~(1LL << k))
 #define CheckBit(x, k) ((x>>k)&1)
+#define dbg(x) cerr << #x << " " << x << "\n";
 const int mod = 998244353;
 const int N  = 2e5+5;
 
 
-
-
 void solve(){
-	
+    
 }
 
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    // freopen("input.in","r",stdin);
+    // freopen("outpu.out","w",stdout);
     int _t = 1;
     cin>>_t;
     for(int i = 1;i<=_t;i++){
-    	// cout<<"Case "<<i<<": ";
-    	solve();
+        // cout<<"Case "<<i<<": ";
+        solve();
     }
 }
