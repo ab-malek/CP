0211cc4b521ea4904af0a101594307113ac727d4
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2025-09-23 13:19:41 +0600

    Update MOs Algorithm.cpp

--- Data Structure/MOs Algorithm.cpp
+++ Data Structure/MOs Algorithm.cpp
@@ -1,66 +1,115 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1e6 + 9, B = 440;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// #define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+// for equal type 'less_equal'
 
+#define ll long long
+#define int long long
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define all(a) a.begin(),a.end()
+// #define flush fflush(stdout) 
+const long long INF = 1e18;
+#define check cout<<"ok"<<nl
+// #define mxheap priority_queue<int>
+// #define mnheap priority_queue<int, vector<int>, greater<int>>
+// #define mxheap2 priority_queue<pair<int,int>>
+// #define mnheap2 priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>
+#define printVec(v) for(int i=0;i<v.size();i++){cout<<v[i]<<" ";}cout<<nl
+#define printVecPair(v) for(auto it : v){cout<<it.first<<" "<<it.second<<nl;}
+// bool cmp(pair<int,int> &a, pair<int,int> &b){return a.second < b.second;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define dbg(x) cerr << #x << " " << x << "\n";
+const int mod = 998244353;
+const int N  = 2e5+5;
+
+const int B = 400;
+int sum = 0;
 struct query {
-  int l, r, id;
+  int l, r, idx;
   bool operator < (const query &x) const {
     if(l / B == x.l / B) return ((l / B) & 1) ? r > x.r : r < x.r;
     return l / B < x.l / B;
   }
-} Q[N];
-int cnt[N], a[N];
-long long sum;
-inline void add_left(int i) {
-  int x = a[i];
-  sum += 1LL * (cnt[x] + cnt[x] + 1) * x;
-  ++cnt[x];
-}
-inline void add_right(int i) {
-  int x = a[i];
-  sum += 1LL * (cnt[x] + cnt[x] + 1) * x;
-  ++cnt[x];
-}
-inline void rem_left(int i) {
-  int x = a[i];
-  sum -= 1LL * (cnt[x] + cnt[x] - 1) * x;
-  --cnt[x];
-}
-inline void rem_right(int i) {
-  int x = a[i];
-  sum -= 1LL * (cnt[x] + cnt[x] - 1) * x;
-  --cnt[x];
-}
-long long ans[N];
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+} ar[N];
 
-  int n, q;
-  cin >> n >> q;
-  for(int i = 1; i <= n; i++) cin >> a[i];
-  for(int i = 1; i <= q; i++) {
-    cin >> Q[i].l >> Q[i].r;
-    Q[i].id = i;
-  }
-  sort(Q + 1, Q + q + 1);
-  int l = 1, r = 0;
-  for(int i = 1; i <= q; i++) {
-    int L = Q[i].l, R = Q[i].r;
-    if(R < l) {
-      while (l > L) add_left(--l);
-      while (l < L) rem_left(l++);
-      while (r < R) add_right(++r);
-      while (r > R) rem_right(r--);
-    } else {
-      while (r < R) add_right(++r);
-      while (r > R) rem_right(r--);
-      while (l > L) add_left(--l);
-      while (l < L) rem_left(l++);
-    }
-    ans[Q[i].id] = sum;
-  }
-  for(int i = 1; i <= q; i++) cout << ans[i] << '\n';
-  return 0;
+void add(int x){
+    sum += x;
 }
+void rem(int x){
+    sum -= x;
+}
+
+void solve(){
+    int n,q;
+    cin>>n>>q;
+    int val[n];
+    for(int i=0;i<n;i++){
+        cin>>val[i];
+    }
+
+    // dbg(val[0]);
+
+    for(int i=0;i<q;i++){
+        cin>>ar[i].l>>ar[i].r;
+        ar[i].idx = i;
+    }
+
+    sort(ar,ar+q);
+
+    vector<int> ans(q+1,0);
+
+    int cur_l = 0, cur_r = -1;
+
+    for(int i=0;i<q;i++){
+        int l = ar[i].l, r = ar[i].r;
+        l--,r--;
+        while(cur_l > l){
+            cur_l--;
+            add(val[cur_l]);
+        }
+        while(cur_r < r){
+            cur_r++;
+            add(val[cur_r]);
+        }
+        while(cur_l < l){
+            rem(val[cur_l]);
+            cur_l++;
+        }
+        while(cur_r > r){
+            rem(val[cur_r]);
+            cur_r--;
+        }
+
+        ans[ar[i].idx] = sum;
+    }
+
+    for(int i=0;i<q;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<nl;
+
+}
+
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.in","r",stdin);
+    // freopen("outpu.out","w",stdout);
+    int _t = 1;
+    // cin>>_t;
+    for(int i = 1;i<=_t;i++){
+        // cout<<"Case "<<i<<": ";
+        solve();
+    }
+}
+// static range sum problem
